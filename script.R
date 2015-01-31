53e3affcc0bb15c54e05bf934220978ddc537fb6
Jirka Mauritz <jirmauritz@gmail.com>
2015-01-31 14:17:47 +0100

    Fixed bugs in template script.

--- script.R
+++ script.R
@@ -37,6 +37,7 @@ for (x in 1:length(files.broadsheet)) {
 documents.tabloid <- vector("character", length(files.tabloid))
 for (x in 1:length(files.tabloid)) {
   file <- readLines(files.tabloid[x], encoding="UTF-8")
+  file <- paste(file[1:length(file)], collapse='\n')
   
   documents.tabloid[x] <- file
 }
@@ -45,6 +46,7 @@ for (x in 1:length(files.tabloid)) {
 documents.broadsheet <- vector("character", length(files.broadsheet))
 for (x in 1:length(files.broadsheet)) {
   file <- readLines(files.broadsheet[x], encoding="UTF-8")
+  file <- paste(file[1:length(file)], collapse='\n')
   
   documents.broadsheet[x] <- file
 }
@@ -55,21 +57,22 @@ documents.broadsheet <- iconv(enc2utf8(documents.broadsheet), sub = "byte")
 
 # create Corpus of tabloids and transform
 corpus.tabloid <- VCorpus(VectorSource(documents.tabloid))
-corpus.tabloid <- tm_map(corpus.tabloid, stripWhitespace)
 corpus.tabloid <- tm_map(corpus.tabloid, removePunctuation)
 corpus.tabloid <- tm_map(corpus.tabloid, removeNumbers)
 corpus.tabloid <- tm_map(corpus.tabloid, content_transformer(tolower))
 corpus.tabloid <- tm_map(corpus.tabloid, removeWords, stopwords("english"))
 corpus.tabloid <- tm_map(corpus.tabloid, stemDocument, language="english")
+corpus.tabloid <- tm_map(corpus.tabloid, stripWhitespace)
 
 # create Corpus of broadsheets and transform
 corpus.broadsheet <- VCorpus(VectorSource(documents.broadsheet))
-corpus.broadsheet <- tm_map(corpus.broadsheet, stripWhitespace)
 corpus.broadsheet <- tm_map(corpus.broadsheet, removePunctuation)
 corpus.broadsheet <- tm_map(corpus.broadsheet, removeNumbers)
 corpus.broadsheet <- tm_map(corpus.broadsheet, content_transformer(tolower))
 corpus.broadsheet <- tm_map(corpus.broadsheet, removeWords, stopwords("english"))
 corpus.broadsheet <- tm_map(corpus.broadsheet, stemDocument, language="english")
+corpus.broadsheet <- tm_map(corpus.broadsheet, stripWhitespace)
+
 
 # DATA FOR EVALUATION
 algorithms <- c("J48", "JRip", "svmLinear", "rf", "knn", "lda", "plr", 
